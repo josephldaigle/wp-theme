e2a07355f317d3277d95278f0fc3ac8086ecf325
Joe Daigle <josephldaigle@yahoo.com>
2018-03-14 22:26:20 -0400

    update wp core install command

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -4,4 +4,4 @@ mv wp-config.php wp/
 
 sudo -su ec2-user
 
-/vendor/bin/wp core install --path='wp' --url=http://wptheme-env.gdyysacpvg.us-east-1.elasticbeanstalk.com/ --title="WP Theme" --admin_user=webmaster --admin_email=josephldaigle@yahoo.com --admin_password="password"
+vendor/bin/wp core install --path='wp' --url=http://localhost --title="WP Theme" --admin_user=webmaster --admin_email=josephldaigle@yahoo.com --admin_password="password"
