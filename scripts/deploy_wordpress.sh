41ae5ff228f9e2d2666b1e5a7aec5baae1684fee
Joe Daigle <josephldaigle@yahoo.com>
2018-03-15 13:11:13 -0400

    wrap wp install in conditional update vhost config to AllowOverrid

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -4,4 +4,6 @@ mv wp-config.php wp/
 
 sudo -su ec2-user
 
-vendor/bin/wp core install --path='wp' --url=http://localhost --title="WP Theme" --admin_user=webmaster --admin_email=josephldaigle@yahoo.com --admin_password="password"
+if ! $(vendor/bin/wp core is-installed); then
+    vendor/bin/wp core install --path='wp' --url=http://localhost --title="WP Theme" --admin_user=webmaster --admin_email=josephldaigle@yahoo.com --admin_password="password"
+fi
