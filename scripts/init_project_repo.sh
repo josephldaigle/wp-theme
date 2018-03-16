60c8e9c31a45eef167d63addf97d2002328711dc
Joe Daigle <josephldaigle@yahoo.com>
2018-03-15 22:32:40 -0400

    wrap git init in conditional

--- scripts/init_project_repo.sh
+++ scripts/init_project_repo.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 cd ../wp
-sudo git init
-sudo git add .
-sudo git commit -m 'initial commit'
+
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+if [ "$inside_git_repo" ]; then
+    echo "inside git repo"
+    # pull master branch
+else
+    sudo git init
+    sudo git add .
+    sudo git commit -m 'initial commit'
+fi
+
+
