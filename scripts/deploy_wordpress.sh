a477183c6e76300d047f3f45578785d880bf18c7
Joe Daigle <josephldaigle@yahoo.com>
2018-03-17 17:26:37 -0400

    wrap script in wp installed check

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 #########################
-# WP Install Script
+# WP Deploy Script
 #########################
 #
 # Script does not run if WP is already installed.
 #
 
-## record working dir
-DIR="$(pwd)"
+if ! $(${DIR}/vendor/bin/wp core is-installed .);
+then
+    ## record working dir
+    DIR="$(pwd)"
 
-## move to wp install dir
-cd ../wp
-echo 'changed pwd to wp install dir'
+    ## move to wp install dir
+    cd ../wp
+    echo 'changed pwd to wp install dir'
 
-## move the wp-config file
-$(sudo mv ${DIR}/wp-config.php .)
-$(sudo mv ${DIR}/.htaccess .)
-echo 'deployed wp-config.php and .htaccess'
+    ## move the wp-config file
+    $(sudo mv ${DIR}/wp-config.php .)
+    $(sudo mv ${DIR}/.htaccess .)
+    echo 'deployed wp-config.php and .htaccess'
 
-## return to orig working dir
-cd ${DIR}
-echo 'returned to original dir'
-echo "$(pwd)"
+    ## return to orig working dir
+    cd ${DIR}
+    echo 'returned to original dir'
+    echo "$(pwd)"
+fi
