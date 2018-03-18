70f8be3b23afa1a3dd068825c8e209324d758c84
Joe Daigle <josephldaigle@yahoo.com>
2018-03-17 21:32:43 -0400

    alter git deploy script

--- scripts/init_project_repo.sh
+++ scripts/init_project_repo.sh
@@ -7,19 +7,31 @@
 # Initialize a git project in the WP install directory.
 #
 
-
+DIR="$(pwd)"
 cd ../wp
 
 inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 
-if [ "$inside_git_repo" ]; then
-    echo "inside git repo"
-    # pull master branch
-else
-    sudo git init
-    sudo git config --local user.name "${CLIENT_NAME}"
-    sudo git config --local user.email "${CLIENT_EMAIL}"
+# configure project
+sudo git config --local user.name "${GIT_NAME}"
+sudo git config --local user.email "${GIT_EMAIL}"
+echo 'git config complete'
 
+if [ "$inside_git_repo" ]; then
+    echo 'inside git repo'
+
+    # pull master branch
+
+else
+    #init git project
+    sudo git init
+    echo 'git initialized'
+
+    # execute initial commit
     sudo git add .
     sudo git commit -m 'initial commit'
+    echo 'project committed'
 fi
+
+## return to orig working dir
+cd ${DIR}
