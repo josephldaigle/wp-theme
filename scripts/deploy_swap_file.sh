8c80172d2a91a43dfad985d647640431e2a6280b
Joe Daigle <josephldaigle@yahoo.com>
2018-03-18 20:05:30 -0400

    - change swap deploy script

--- scripts/deploy_swap_file.sh
+++ scripts/deploy_swap_file.sh
@@ -8,15 +8,16 @@
 # https://wiki.archlinux.org/index.php/swap
 #
 
-echo 'checking swap file...'
+echo 'checking swap'
 
 if ! free | awk '/^Swap:/ {exit !$2}';
 then
-
     sudo dd if=/dev/zero of=/swapspace bs=1M count=4000
 
     sudo mkswap /swapspace
     sudo swapon /swapspace
 
     echo 'swap file created'
+else
+    echo 'swap found'
 fi
