2d30e1bc69c818003dbb30c1ed98f56d7498fa75
josephldaigle <josephldaigle@yahoo.com>
2018-07-07 20:25:17 -0400

    Minor edit

--- scripts/deploy_wordpress_db.sh
+++ scripts/deploy_wordpress_db.sh
@@ -11,11 +11,11 @@
 
 echo 'deploying wordpress database'
 
-#mysql -h${RDS_HOSTNAME} -P${RDS_PORT} -u${RDS_USERNAME} -p${RDS_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS ebdb; GRANT ALL ON ebdb.* TO wp_user IDENTIFIED BY '${RDS_PASSWORD}'; FLUSH PRIVILEGES;" ${RDS_DB_NAME}
+#mysql -h${RDS_HOSTNAME} -P${RDS_PORT} -u${RDS_USERNAME} -p${RDS_PASSWORD} -e" CREATE DATABASE IF NOT EXISTS ebdb; GRANT ALL ON ebdb.* TO wp_user IDENTIFIED BY '${RDS_PASSWORD}'; FLUSH PRIVILEGES;" ${RDS_DB_NAME}
 
 # try to connect to database
 #if [ ! mysql -u root -e 'USE ${RDS_DB_NAME};' ]; then
-#  echo 'database does not e'
+#  echo 'database does not exist
 #fi
 #
 #if [ "`mysql -h${RDS_HOSTNAME} -P${RDS_PORT} -u${RDS_USERNAME} -p${RDS_PASSWORD} -se'USE ebdb;' 2>&1`" == "" ]; then
@@ -23,7 +23,3 @@ echo 'deploying wordpress database'
 #else
 #    echo 'ebdb exists'
 #fi
-
-
-## travis cmd
-#mysql -h${RDS_HOSTNAME} -P${RDS_PORT} -u${RDS_USERNAME} -p${RDS_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS ebdb; GRANT ALL ON ebdb.* TO wp_user IDENTIFIED BY '$RDS_PASSWORD'; FLUSH PRIVILEGES;"
